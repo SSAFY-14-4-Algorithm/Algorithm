7890962aa44e3cd2927741fab884b322ce8ee7f1
jung980514 <jung980514@gmail.com>
2025-02-02 13:24:53 +0900

    Create Branch

---
+++ 1주차/src/Baekjoon_3986.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon_3986 {
+	public BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		System.out.println("Hello World");;
+	}
+}
