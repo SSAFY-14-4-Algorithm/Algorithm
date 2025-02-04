383f444164a5f48db00d82757ec313ee52a4b54b
jaewook <jj99526@naver.com>
2025-02-04 10:48:59 +0900

    feat : 좋은단어 카드놓기

--- week1/Baekjoon18115.java
+++ week1/Baekjoon18115.java
@@ -1,5 +1,49 @@
 package week1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
 public class Baekjoon18115 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int num=Integer.parseInt(br.readLine());
+        int nums[]=new int[num];
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        StringBuilder stringBuilder=new StringBuilder();
+        Deque<Integer> deque=new LinkedList<>();
+
+        for(int i=num-1;i>=0;i--){
+            nums[i]=Integer.parseInt(st.nextToken());;
+        }
+        for(int i=0;i<num;i++) {
+            switch (nums[i]) {
+                case 1: {
+                    deque.addFirst(i+1);
+                    break;
+                }
+                case 2: {
+                    int top = deque.removeFirst();
+                    deque.addFirst(i+1);
+                    deque.addFirst(top);
+                    break;
+                }
+                case 3: {
+                    deque.addLast(i+1);
+                    break;
+                }
+            }
+        }
+        for(int i=0;i<num;i++){
+            stringBuilder.append(deque.pop()+" ");
+        }
+        System.out.println(stringBuilder);
+
+    }
 
 }
--- week1/Baekjoon3986.java
+++ week1/Baekjoon3986.java
@@ -1,5 +1,44 @@
 package week1;
+import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Baekjoon3986 {
 
+    //A와 B가 겹치지않는 선으로 연결되면 된다
+    //문자를 스택에 넣는다
+    //넣을때 마다 맨위에 값을 가져와 넣는 문자와 같으면 pop
+    //아닐시 add 한다
+    //스택이 비여있으면 카운트를 1 상승시킨다
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int result=0;
+        int T=Integer.parseInt(br.readLine());
+
+        for(int i=0;i<T;i++){
+            String word=br.readLine();
+            int wordLength=word.length();
+            Stack<Character> stack=new Stack<>();
+            for(int j=0;j<wordLength;j++){
+                if(!stack.empty()&&stack.peek()==word.charAt(j)){
+                    stack.pop();
+                }else{
+                    stack.add(word.charAt(j));
+                }
+            }
+            if(stack.empty()){
+                result++;
+            }
+        }
+        System.out.println(result);
+
+
+
+    }
 }
