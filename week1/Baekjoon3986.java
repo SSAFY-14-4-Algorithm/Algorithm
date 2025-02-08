a59d09c93f38b3a825f37e7b5043b171b20be911
ssongmim <108718162+ssongmim@users.noreply.github.com>
2025-02-09 02:20:18 +0900

    Update Baekjoon3986.java

--- week1/Baekjoon3986.java
+++ week1/Baekjoon3986.java
@@ -1,5 +1,35 @@
 package week1;
 
 public class Baekjoon3986 {
+  public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		//스택 활용
+		
+		int N = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		
+		for(int i=0;i<N;i++) {
+			Stack<Character> stack = new Stack<>();
+			String line = br.readLine();
+			for(char c : line.toCharArray()) {
+				if(!stack.isEmpty() && stack.peek()==c) {
+					stack.pop();
+				} else {
+					stack.push(c);
+				}
+			}
+			if(stack.isEmpty()) {
+				cnt++;
+			}
+		}
+		
+		bw.write(cnt+"\n");
+		bw.flush();
+		br.close();
+		bw.close();
+	
 
+	}
 }
