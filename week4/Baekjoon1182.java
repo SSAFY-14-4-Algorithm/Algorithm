441cd98611502c9a2df946c23036f6f984846b4f
zeus <jj99526@naver.com>
2025-02-27 21:10:21 +0900

    feat : 2문제

--- week4/Baekjoon1182.java
+++ week4/Baekjoon1182.java
@@ -1,7 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int[] arr;
+	static int N;
+	static int S;
+	static int count;
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		//부분집합
+
+		subset(0, 0,0);
+		System.out.println(count);
+
 	}
+
+	static void subset(int start, int sum,int cnt) {
+
+		if (start == N) {
+			if (sum == S  && cnt>0) {
+				count++;
+			}
+			return;
+		}
+		subset(start + 1, sum,cnt);
+
+		subset(start + 1, sum + arr[start],cnt+1);
+
+
+	}
+
 }
--- week4/Baekjoon14889.java
+++ week4/Baekjoon14889.java
@@ -1,7 +1,88 @@
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
-		
+public class Main {
+	static int[][] arr;
+	static boolean[] visited;
+	static int N;
+	static int min=Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		N=Integer.parseInt(br.readLine());
+		arr=new int[N][N];
+		visited=new boolean[N];
+		for(int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++){
+				arr[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		combination(0,0,visited);
+
+		// 4개를 조합으로 뽑는 경우를 하고
+		// 2개씩 확인
+		System.out.println(min);
+
+
 	}
+
+	private static void combination(int start, int cnt, boolean[] visited) {
+		if(cnt==N/2){
+			min=Math.min(cal(),min);
+			return;
+		}
+
+		for(int i=start; i<N;i++){
+			visited[i]=true;
+			combination(i+1,cnt+1,visited);
+			visited[i]=false;
+
+		}
+
+	}
+	static int cal(){
+		int startTeamSum=0;
+		int linkTeamSum=0;
+
+		for(int i=0;i<N-1;i++){
+			for(int j=i+1;j<N;j++){
+				if(visited[i]==true&&visited[j]==true){
+					startTeamSum=startTeamSum+arr[i][j];
+					startTeamSum=startTeamSum+arr[j][i];
+				}else if(visited[i]==false&&visited[j]==false){
+					linkTeamSum=linkTeamSum+arr[i][j];
+					linkTeamSum=linkTeamSum+arr[j][i];
+				}
+			}
+		}
+		return Math.abs(startTeamSum-linkTeamSum);
+	}
+
+
+	//기저조건
+	//cnt==4일떄
+
+	//반복문
+	//다음조합을 선택
+//    static void combination(int start,int cnt,int[][] result){
+//        if(cnt==N){
+//            return;
+//        }
+//        for(int i=0;i<N;i++){
+//            for(int j=start;j<N;j++){
+////                System.out.println("i = "+i+" j = "+j);
+//                if(i==j)
+//                    continue;
+//                result[cnt][0]=i;
+//                result[cnt][1]=j;
+//                combination(j+1,cnt+1,result);
+//                System.out.println();
+//            }
+//        }
+//    }
 }
