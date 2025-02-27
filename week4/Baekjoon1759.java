3d33cc6b580256145b82e1f93989d3f7adb480e7
gd10080008 <gd10080008@gmail.com>
2025-02-27 11:34:21 +0900

    Submit

--- week4/Baekjoon1759.java
+++ week4/Baekjoon1759.java
@@ -1,7 +1,53 @@
+import java.io.*;
+import java.util.*;
+public class Baekjoon1759 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-public class Main {
+    static int L,C;
+    static String[] alphabets;
+    static String[] output;
+    static ArrayList<String> answers = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        String [] tokens = br.readLine().split(" ");
+        L = Integer.parseInt(tokens[0]);
+        C = Integer.parseInt(tokens[1]);
+        alphabets = new String[C];
+        output = new String[L];
+        tokens = br.readLine().split(" ");
+        for(int i=0;i<C;i++){
+            alphabets[i] = tokens[i];
+        }
+        Arrays.sort(alphabets);
+        backtracking(0,0,0,0,"");
+        Collections.sort(answers);
+        for (String answer : answers) {
+            bw.write(answer+"\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    private static void backtracking(int depth, int index,int vowels, int consonants,String password){
+        if(depth == L){
+            if(vowels >= 1 && consonants >= 2 ){
+                answers.add(password);
+            }
+            return;
+        }
 
-	public static void main(String[] args) {
-		
-	}
+        if(index >= C) return;
+
+        char currentChar = alphabets[index].charAt(0);
+        if(isVowel(currentChar)){
+            backtracking(depth+1,index+1,vowels+1,consonants,password+alphabets[index]);
+        }else{
+            backtracking(depth+1,index+1,vowels,consonants+1,password+alphabets[index]);
+        }
+        //현재 보고 있는 character를 스킵
+        backtracking(depth,index+1,vowels,consonants,password);
+    }
+    private static boolean isVowel(char ch){
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
 }
