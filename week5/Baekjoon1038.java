c4728fd692cd338a45e1adf95d445916672c1632
JJaeWooks <100128772+JJaeWooks@users.noreply.github.com>
2025-03-09 21:10:28 +0900

    Update Baekjoon1038.java

--- week5/Baekjoon1038.java
+++ week5/Baekjoon1038.java
@@ -1,7 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-	}
+  static List<Long> list = new ArrayList<>();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    if (N > 1022) {
+      System.out.println(-1);
+      System.exit(0);
+    }else {
+      for (int i = 0; i <= 9; i++) {
+        permutation(1, i);
+      }
+//      System.out.println(list);
+
+      Collections.sort(list);
+      System.out.println(list.get(N));
+    }
+  }
+
+  static void permutation(int cnt, long num) {
+
+    if (cnt > 10) {
+      return;
+    }
+    list.add(num);
+    for (int i = 0; i < num % 10; i++) {
+      permutation(cnt + 1, (num * 10) + i);
+    }
+  }
+
 }
