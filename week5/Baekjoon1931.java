c9310035da40ced54b3be493fd075fbb759cde96
minhe8564 <minhe8564@gmail.com>
2025-03-09 21:58:33 +0900

    코드 제출

--- week5/Baekjoon1931.java
+++ week5/Baekjoon1931.java
@@ -1,7 +1,41 @@
+package algorithm;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+public class Baekjoon1931 {
+	static int N;
+	static int[][] time;
+	static int cnt = 0;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		time = new int[N][2];
 		
+		for(int n = 0; n < N; n++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			time[n][0] = Integer.parseInt(st.nextToken());
+			time[n][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(time, (o1, o2) -> {
+			if(o1[1]==o2[1])
+				return o1[0]-o2[0];
+			else 
+				return o1[1]-o2[1];
+		});
+//		System.out.print(Arrays.deepToString(time));
+		
+		int end = 0;
+		for(int n = 0; n < N; n++) {
+			if(time[n][0] >= end) {
+				cnt++;
+				end = time[n][1];
+			}
+		}
+		
+		System.out.print(cnt);
 	}
+
 }
--- week5/Baekjoon9663.java
+++ week5/Baekjoon9663.java
@@ -1,7 +1,65 @@
+package algorithm;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+public class Baekjoon9663 {
+	static int N;
+	static int[][] board;
+	static int cnt;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
+		
+		backTracking(0);
+
+		sb.append(cnt);
+		System.out.print(sb);
+		br.close();
+	}
+
+	private static void backTracking(int idx) {
+		if (idx == N) {
+			cnt++;
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if(board[idx][i] != 0) {
+				continue;
+			}
+			
+			setQueen(idx, i, 1); // 선택, 배치 한 칸 +1
+			backTracking(idx+1);
+			setQueen(idx, i, -1); // 선택안함, 배치 안 한 칸 -1
+		}
+	}
+	
+	private static void setQueen(int row, int col, int command) {
+        // 같은 열 체크
+		for(int i = 0; i < N; i++) {
+			board[i][col] += command; 
+		}
+		
+		// 대각선 방향 체크
+		for(int i = 0; i < N; i++) {
+			if(row+i < N && col+i < N) {
+				board[row+i][col+i] += command;
+			}
+			if(row-i >= 0 && col+i < N) {
+				board[row-i][col+i] += command;
+			}
+			if(row+i < N && col-i >= 0) {
+				board[row+i][col-i] += command;
+			}
+			if(row-i >= 0 && col-i >= 0){
+				board[row-i][col-i] += command;
+			}
+		}
 		
 	}
+	
+
 }
