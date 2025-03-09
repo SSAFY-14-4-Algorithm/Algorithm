b6db2b2aa5944306d7262c63201e66b3d083e08e
JJaeWooks <100128772+JJaeWooks@users.noreply.github.com>
2025-03-09 21:09:58 +0900

    Update Baekjoon16953.java

--- week5/Baekjoon16953.java
+++ week5/Baekjoon16953.java
@@ -1,7 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+  static int result=-1;
+  static boolean check;
+  static Long min;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) {
-		
-	}
+
+    int A=Integer.parseInt(st.nextToken());
+    int B=Integer.parseInt(st.nextToken());
+    min=Long.MAX_VALUE;
+
+    dfs(A,B,1);
+    if (check) {
+      System.out.println(min);
+    } else {
+      System.out.println(-1);
+    }
+  }
+  static void dfs(long a,int b,int cnt){
+    if(a>b)
+      return;
+    if(a==b){
+      min=Math.min(min,cnt);
+      check=true;
+      return;
+    }
+    dfs(a*10+1,b,cnt+1);
+
+    dfs(a*2,b,cnt+1);
+  }
+
 }
+//1000000000
