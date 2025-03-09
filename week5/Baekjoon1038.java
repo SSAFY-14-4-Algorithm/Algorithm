35903d0e7b9db1af18ff1fc6299e63ecc0cf1ee5
ssongmim <108718162+ssongmim@users.noreply.github.com>
2025-03-09 16:48:12 +0900

    Update Baekjoon1038.java

--- week5/Baekjoon1038.java
+++ week5/Baekjoon1038.java
@@ -3,44 +3,41 @@ import java.util.*;
 
 
 public class Main {
-	public static class Node{
-		long x;
-		int depth;
-		public Node(long x, int depth) {
-			this.x = x;
-			this.depth = depth;
-		}
-		
-	}
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
 		
-		long A = Integer.parseInt(st.nextToken());
-		long B = Integer.parseInt(st.nextToken());
+		List<Long> Numbers = new ArrayList<>(); //감소하는 수 목록을 미리 모두 작성
+		makeNumbers(Numbers);
 		
-		
-		System.out.println(bfs(A,B));
-		
+		if(N>= Numbers.size()) {
+			System.out.println(-1);
+		}else {
+			System.out.println(Numbers.get(N));
+		}
 	}
 	
-	public static int bfs(long A, long B) {
-		ArrayDeque<Node> queue = new ArrayDeque<>();
-		queue.add(new Node(A,1));
+	public static void makeNumbers(List<Long> list) {
+		Queue<Long> queue = new LinkedList<>();
 		
-		while(!queue.isEmpty()) {
-			Node cur = queue.poll();
-			long x = cur.x;
-			int depth = cur.depth;
-			
-			if(x==B) return depth;
-			
-			if(x*2<=B) queue.add(new Node(x*2,depth+1));
-			if(x*10+1<=B) queue.add(new Node(x*10+1,depth+1));
-			
+		for (int i=0;i<=9;i++) { //큐에 0~9를 넣어 감소하는 수를 만들기 위해서 기반을 만들어 두는 것 
+			queue.offer((long)i);
+			list.add((long) i);
 		}
 		
-		return -1;
+		while(!queue.isEmpty()) { // 감소하는 수를 생성하기 (bfs이용)
+			long num = queue.poll();
+			long lastDigit = num%10; // 마지막 자리 숫자가 뭔지 알기 위해서 수
+			
+			for(int i=0;i<lastDigit;i++) { //마지막 자리보다 작은 수를 맨 뒤에 붙여서 새로운 감소하는 수를 생
+				long newNum = num*10+i;
+				queue.offer(newNum);
+				list.add(newNum);
+			}
+		}
+		
+		Collections.sort(list); //만든 감소하는 수의 리스트를 오름차순으로 정
 		
 	}
 }
