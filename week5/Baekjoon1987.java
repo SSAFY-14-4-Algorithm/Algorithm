059f6ad53bf118a1e52e12278754aee5d9ef373f
ssongmim <108718162+ssongmim@users.noreply.github.com>
2025-03-09 16:53:51 +0900

    Update week5

--- week5/Baekjoon1987.java
+++ week5/Baekjoon1987.java
@@ -1,46 +1,7 @@
-import java.io.*;
-import java.util.*;
 
 public class Main {
-    static int R, C, maxCount = 0;
-    static char[][] board;
-    static boolean[] visited = new boolean[26];
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        board = new char[R][C];
-
-        for (int i = 0; i < R; i++) {
-            board[i] = br.readLine().toCharArray();
-        }
-
-        visited[board[0][0] - 'A'] = true;
-        dfs(0, 0, 1);
-        
-        System.out.println(maxCount);
-    }
-
-    static void dfs(int x, int y, int count) {
-        maxCount = Math.max(maxCount, count);
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && ny >= 0 && nx < R && ny < C) {
-                int index = board[nx][ny] - 'A';
-                if (!visited[index]) {
-                    visited[index] = true;
-                    dfs(nx, ny, count + 1);
-                    visited[index] = false;
-                }
-            }
-        }
-    }
+	public static void main(String[] args) {
+		
+	}
 }
