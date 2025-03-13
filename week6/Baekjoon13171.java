29e9b560c9db014d3c00891792e51d37a0fab199
ssongmim <108718162+ssongmim@users.noreply.github.com>
2025-03-13 17:03:27 +0900

    Update Baekjoon13171.java

--- week6/Baekjoon13171.java
+++ week6/Baekjoon13171.java
@@ -1,7 +1,31 @@
+package Baekjoon;
 
-public class Main {
+import java.io.*;
+import java.util.*;
+/*
+ * 메모리: 14156KB
+ * 시간: 100ms
+ */
 
-	public static void main(String[] args) {
-		
-	}
+public class Baekjoon13171{
+    static final long Mod = 1000000007;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long A = Long.parseLong(br.readLine()); 
+        long X = Long.parseLong(br.readLine());
+        
+        System.out.println(modCal(A, X));
+    }
+    
+    static long modCal(long A, long X) {
+        long res = 1;
+        A%=Mod;
+        while (X > 0) {
+            if (X % 2 == 1) res = (res * A) % Mod;
+            A = (A * A) % Mod;
+            X /= 2;
+        }
+        return res;
+    }
 }
