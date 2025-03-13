b54aeeb34bed9f7ed2fae491bc3bd38e8cef5c6c
jung980514 <jung980514@naver.com>
2025-03-13 10:50:18 +0900

    Update week6

---
+++ week6/Baekjoon17070.java
@@ -0,0 +1,120 @@
+import java.io.*;
+import java.util.*;
+/*
+ * 메모리 16168KB
+ * 시간 312ms
+ */
+public class Baekjoon17070 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dx = {0,1,1};
+	static int[] dy = {1,1,0};	
+	static int result = 0;
+	public static void main(String[] args) throws IOException{
+		int n = Integer.parseInt(br.readLine());
+		map = new int[n][n];
+		visited = new boolean[n][n];
+		for(int i=0;i<n;i++) {
+			String[] s = br.readLine().split(" ");
+			for(int j=0;j<n;j++) {
+				map[i][j] = Integer.parseInt(s[j]);
+			}
+		}
+		int flag = 0; // 0:가로 1:대각선  2:세로
+		//시작 파이프 위치 방문처리
+		visited[0][0]=true;
+		visited[0][1]=true;
+		
+		dfs(0,1,flag,n);
+		System.out.println(result);
+	}
+	public static void dfs(int x,int y,int flag,int n) {
+		if(x==n-1 && y==n-1) { // (n,n)값이면 방법 수 +1
+			result++;
+			return;
+		}
+		if(flag==0) { //가로일때
+			for(int i=0;i<2;i++) {
+				int nx = x+dx[i];
+				int ny = y+dy[i];
+				if(i==1) { //대각선일때는 (nx,y),(x,ny),(nx,ny) 3개를 다 비교해야한다
+					if(isDiag(x,y,nx,ny,n)) {
+						visited[nx][ny] = true;
+						visited[nx][y] = true;
+						visited[x][ny] = true;
+						dfs(nx,ny,i,n);
+						visited[nx][ny] = false;
+						visited[nx][y] = false;
+						visited[x][ny] = false;
+					}
+				}
+				else {
+					if(isRange(nx,ny,n) && !visited[nx][ny] && map[nx][ny]==0) {
+						visited[nx][ny] = true;
+						dfs(nx,ny,i,n);
+						visited[nx][ny] = false;
+					}
+				}
+			}
+		}
+		else if(flag==1) { //대각선일때
+			for(int i=0;i<3;i++) {
+				int nx = x+dx[i];
+				int ny = y+dy[i];
+				if(i==1) { //대각선일때는 (nx,y),(x,ny),(nx,ny) 3개를 다 비교해야한다
+					if(isDiag(x,y,nx,ny,n)) {
+						visited[nx][ny] = true;
+						visited[nx][y] = true;
+						visited[x][ny] = true;
+						dfs(nx,ny,i,n);
+						visited[nx][ny] = false;
+						visited[nx][y] = false;
+						visited[x][ny] = false;
+					}
+				}
+				else {
+					if(isRange(nx,ny,n) && !visited[nx][ny] && map[nx][ny]==0) {
+						visited[nx][ny] = true;
+						dfs(nx,ny,i,n);
+						visited[nx][ny] = false;
+					}
+				}
+			}
+		}
+		else { // 세로일때
+			for(int i=1;i<3;i++) {
+				int nx = x+dx[i];
+				int ny = y+dy[i];
+				if(i==1) { //대각선일때는 (nx,y),(x,ny),(nx,ny) 3개를 다 비교해야한다
+					if(isDiag(x,y,nx,ny,n)) {
+						visited[nx][ny] = true;
+						visited[nx][y] = true;
+						visited[x][ny] = true;
+						dfs(nx,ny,i,n);
+						visited[nx][ny] = false;
+						visited[nx][y] = false;
+						visited[x][ny] = false;
+					}
+				}
+				else {
+					if(isRange(nx,ny,n) && !visited[nx][ny] && map[nx][ny]==0) {
+						visited[nx][ny] = true;
+						dfs(nx,ny,i,n);
+						visited[nx][ny] = false;
+					}
+				}
+			}
+		}
+	}
+	public static boolean isRange(int x,int y,int n) { //nx와 ny값이 범위안인지 판별
+		if(x>=0 && y>=0 && x<n && y<n) return true;
+		else return false;
+	}
+	public static boolean isDiag(int x,int y,int nx,int ny,int n) { // 대각선이 가능한지 판별
+		if(isRange(nx,ny,n) && !visited[nx][y] && !visited[x][ny] 
+				&& !visited[nx][ny] && map[nx][ny]==0 && map[nx][y]==0
+						&& map[x][ny]==0) return true;
+		else return false;
+	}
+}
---
+++ week6/Baekjoon2623.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+/*
+ * 메모리 15108KB
+ * 시간 128ms
+ */
+public class Baekjoon2623 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[] in; //진입차수 저장
+	static boolean[] visited;
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	public static void main(String[] args) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		String[] s = br.readLine().split(" ");
+		int n = Integer.parseInt(s[0]);
+		int m = Integer.parseInt(s[1]);
+		for(int i=0;i<=n;i++) {
+			graph.add(new ArrayList<>());
+		}
+		in = new int[n+1];
+		visited = new boolean[n+1];
+		for(int i=0;i<m;i++) {
+			String[] s2 = br.readLine().split(" ");
+			for(int j=1;j<Integer.parseInt(s2[0]);j++) {
+				graph.get(Integer.parseInt(s2[j])).add(Integer.parseInt(s2[j+1]));
+				in[Integer.parseInt(s2[j+1])]++;
+			}
+		}
+		int count=0;
+		//Topological Sorting
+		Queue<Integer> q = new ArrayDeque<Integer>();
+		for(int i=1;i<=n;i++) {
+			//진입 차수 0인 노드 큐에 저장
+			if(in[i]==0) {
+				q.offer(i);	
+				visited[i] =true;
+			}
+		}
+		while(!q.isEmpty()) {
+			int temp = q.poll();
+			//순서정해진 가수 세기
+			sb.append(temp+"\n");
+			count++;
+			//해당 노드와 연결된 간선 제거(연결된 노드 진입차수 -1)
+			for(int node : graph.get(temp)) {
+				in[node]--;
+			}
+			//진입차수가 0이고 방문하지않은 노드 큐에 저장
+			for(int i=1;i<=n;i++) {
+				if(in[i]==0 && !visited[i]) {
+					q.offer(i);	
+					visited[i] = true;
+				}
+			}
+		}
+		//가수의 수만큼 순서 정해졌다면 출력 불가능하면 0출력
+		if(count==n) System.out.print(sb.toString());
+		else System.out.println(0);
+		
+	}
+}
