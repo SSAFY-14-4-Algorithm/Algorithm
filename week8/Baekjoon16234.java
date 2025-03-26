0e7b0ac1ebd731e9e00da679ab20db4089029871
m0304s <cms8955@naver.com>
2025-03-26 20:06:16 +0900

    Update

--- week8/Baekjoon16234.java
+++ week8/Baekjoon16234.java
@@ -1,7 +1,113 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Main {
+public class Baekjoon16234 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int [] dx = {0,0,-1,1};
+    private static int [] dy = {-1,1,0,0};
 
-	public static void main(String[] args) {
-		
-	}
+    private static int N,R,L;
+    private static int [][] population;
+    private static boolean [][] visited;
+
+    public static void main(String[] args) throws IOException {
+        String [] tokens = br.readLine().split(" ");
+        N = Integer.parseInt(tokens[0]);
+        L = Integer.parseInt(tokens[1]);
+        R = Integer.parseInt(tokens[2]);
+
+        population = new int[N][N];
+
+        for(int i=0;i<N;i++){
+            tokens = br.readLine().split(" ");
+            for(int j=0;j<N;j++){
+                population[i][j] = Integer.parseInt(tokens[j]);
+            }
+        }
+
+        int day = 0;
+        while (true){
+            visited = new boolean[N][N];
+            boolean moved = false;
+            for(int i=0;i<N;i++){
+                for(int j=0;j<N;j++){
+                    if(visited[i][j]) continue;
+
+                    if(bfs(i,j)){
+                        moved = true;
+                    }
+                }
+            }
+            if(!moved) break;
+            day++;
+        }
+
+        bw.write(day+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static boolean bfs(int x,int y){
+        Queue<Node> queue = new ArrayDeque<>();
+        LinkedList<Node> union = new LinkedList<>();
+        Node startNode = new Node(x,y);
+        union.add(startNode);
+        queue.add(startNode);
+        visited[x][y] = true;
+
+        int totalPopulation = population[x][y];
+        while (!queue.isEmpty()){
+            Node curNode = queue.poll();
+
+            for(int d=0;d<4;d++){
+                int nx = curNode.x + dx[d];
+                int ny = curNode.y + dy[d];
+
+                if(!inRange(nx,ny) || visited[nx][ny] || !checkInRange(population[curNode.x][curNode.y],population[nx][ny])) continue;
+
+                totalPopulation += population[nx][ny];
+                Node newNode = new Node(nx,ny);
+                queue.add(newNode);
+                union.add(newNode);
+                visited[nx][ny] = true;
+            }
+        }
+
+        if(union.size() > 1){
+            int newPopulation = totalPopulation / union.size();
+            for(Node node : union){
+                population[node.x][node.y] = newPopulation;
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkInRange(int i, int j) {
+        int diff = Math.max(i,j) - Math.min(i,j);
+
+        return diff >= L && diff <= R;
+    }
+
+    private static boolean inRange(int x,int y){
+        return x >= 0 && x < N && y >= 0 && y < N;
+    }
+
+    private static class Node {
+        int x, y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
--- week9/Baekjoon1138.java
+++ week9/Baekjoon1138.java
@@ -1,7 +1,62 @@
+import java.io.*;
+import java.util.Arrays;
 
-public class Main {
+public class Baekjoon1956 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final int MAX = 987654321;
+    private static int V, E;
+    private static int[][] cost;
 
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) throws IOException {
+        String[] tokens = br.readLine().split(" ");
+        V = Integer.parseInt(tokens[0]);
+        E = Integer.parseInt(tokens[1]);
+        cost = new int[V + 1][V + 1];
+
+        // 초기화
+        for (int i = 1; i <= V; i++) {
+            Arrays.fill(cost[i], MAX);
+            cost[i][i] = 0;
+        }
+
+        // 간선 정보 입력
+        for (int i = 0; i < E; i++) {
+            tokens = br.readLine().split(" ");
+            int a = Integer.parseInt(tokens[0]);
+            int b = Integer.parseInt(tokens[1]);
+            int c = Integer.parseInt(tokens[2]);
+
+            cost[a][b] = c;
+        }
+
+        // 플로이드-워셜 알고리즘
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    if (cost[i][j] > cost[i][k] + cost[k][j]) {
+                        cost[i][j] = cost[i][k] + cost[k][j];
+                    }
+                }
+            }
+        }
+
+        // 최소 사이클 찾기
+        int ans = MAX;
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if(i == j) continue;
+                if (cost[i][j] != MAX && cost[j][i] != MAX) {
+                    ans = Math.min(ans, cost[i][j] + cost[j][i]);
+                }
+            }
+        }
+
+        if (ans == MAX) bw.write("-1\n");
+        else bw.write(ans + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
