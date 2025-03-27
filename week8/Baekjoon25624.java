f19e57a7ac83a163c1c9de3ff99f61a0d87ddc4e
LJW9354 <dlwodnjs9354@naver.com>
2025-03-27 21:11:39 +0900

    제출

--- week8/Baekjoon25624.java
+++ week8/Baekjoon25624.java
@@ -1,40 +1,60 @@
-package ct2;
 import java.util.*;
 import java.io.*;
+
 /*
  * 메모리:18868kb, 160ms
  */
 public class Baekjoon25624 {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		for(int testCase = 0; testCase < T; testCase++) {
-			int N = Integer.parseInt(br.readLine());
-			st = new StringTokenizer(br.readLine());
-			int[] arr = new int[N+1];
-			int[][] dp = new int[N+1][N+1];
-			for(int i = 1; i < N+1; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-				arr[i] += arr[i-1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		HashSet<String> hs = new HashSet<>(); //중복 체크
+		boolean flag = true;
+		int[] alphabet = new int[N];
+		int[] used = new int[26];
+		A:for(int i = 0; i < M; i++) {
+			String s = br.readLine();
+			if(hs.contains(s)) {
+				flag = false;
+				break;
 			}
-			for(int i = 2; i <= N; i++) {
-				int len = N-i+1;
-				for(int j = 1; j <= len; j++) {
-					int minValue = Integer.MAX_VALUE;
-					int end = j+i-1;
-					for(int k = j; k < end; k++) {
-						minValue = Math.min(minValue, dp[j][k] + dp[k+1][end]);
-					}
-					dp[j][end] = minValue + arr[end]-arr[j-1];
+			hs.add(s);
+			for(int j = 0; j < N; j++) {
+				char c = s.charAt(j);
+				if(used[c-'A'] == 0) {
+					used[c-'A'] = j+1;
+				} else if(used[c-'A'] != j+1) {
+					flag = false;
+					break A;
 				}
+				alphabet[j] |= (1<<(c-'A'));
 			}
-			for(int i = 1; i <= N; i++) {
-				System.out.println(Arrays.toString(dp[i]));
-			}
-			sb.append(dp[1][N]).append("\n");
 		}
-		System.out.print(sb);
+		int cnt = 1;
+		if(flag) {
+			for(int i = 0; i < N; i++) {
+				cnt *= Integer.bitCount(alphabet[i]);
+			}
+			if(cnt != M) {
+				flag = false;
+			}
+		}
+		if(flag) {
+			StringBuilder sb = new StringBuilder();
+			sb.append("YES").append("\n");
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < 26; j++) {
+					if((alphabet[i] & (1<<j)) != 0) {
+						sb.append((char)(j + 'A'));
+					}
+				}
+				sb.append("\n");
+			}
+			System.out.print(sb);
+		} else {
+			System.out.println("NO");
+		}
 	}
 }
