4d7ee790af9bf5e43df9ad08baa423f8ba3726f6
minhe8564 <minhe8564@gmail.com>
2025-04-03 21:05:07 +0900

    코드 제출

--- week9/Baekjoon1535.java
+++ week9/Baekjoon1535.java
@@ -1,7 +1,48 @@
+/*
+ * BOJ 1535번 : 안녕
+ * 메모리 : 11,612kb
+ * 시간 : 72ms
+ */
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int N;
+	static int[] blood;
+	static int[] happy;
+	static int[] dp;
+	static int maxHappy;
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		blood = new int[N];
+		happy = new int[N];
+		dp = new int[100];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			blood[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			happy[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 99; j >= blood[i]; j--) {
+				dp[j] = Math.max(dp[j], dp[j - blood[i]] + happy[i]);
+			}
+		}
+
+		maxHappy = 0;
+		for (int i = 0; i < 100; i++) {
+			maxHappy = Math.max(maxHappy, dp[i]);
+		}
+
+		sb.append(maxHappy);
+		System.out.print(sb);
+		br.close();
 	}
 }
--- week9/Baekjoon4195.java
+++ week9/Baekjoon4195.java
@@ -1,125 +1,65 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+public class Baekjoon4195 {
+	static int F;
+	static Map<String, Integer> map;
+	static int[] parent;
+	static int[] size;
 
-/**
- * 
- * 각 지점을 중심으로 dfs 아래, 왼쪽, 오른쪽 방향으로만 dfs 탐색 depth:4로
- * 	- 탐색 시 최대값을 발견한다면 갱신
- * 탐색한 지점은 방문 처리
- * 
- * 
- * 
- * 가운데 모양은 따로 탐색 필요
- * 
- * 
- * @author SSAFY
- *
- */
-public class Main_B_14500_테트로미노_연민호 {
-	
-	//하좌우상
-	final static int[] dr = {1, 0, 0, -1};
-	final static int[] dc = {0, -1, 1, 0};
-	
-	
-	
-	static int N;	//세로 크기
-	static int M;	//가로 크기
-	
-	static int[][] map;	//숫자판 정보
-	
-	static boolean[][] visited;	//방문체크
-	
-	static int max;	//최댓값
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		//탐색의 편이를 위해 공백을 넣어놓음
-		map = new int[N+2][M+2];
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			F = Integer.parseInt(br.readLine());
+
+			parent = new int[F*2];
+			size = new int[F*2];
+			for(int f = 0; f < F*2; f++) {
+				parent[f] = f;
+				size[f] = 1;
+			}
+
+			map = new HashMap<String, Integer>();
+			int idx = 0;
+			for (int f = 0; f < F; f++) {
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+				String user1 = st.nextToken();
+				String user2 = st.nextToken();
+				if (!map.containsKey(user1)) {
+					map.put(user1, idx++);
+				}
+				if (!map.containsKey(user2)) {
+					map.put(user2, idx++);
+				}
+
+				union(map.get(user1), map.get(user2));
+
+				int cnt = size[find(map.get(user1))];
+				sb.append(cnt).append("\n");
 			}
 		}
-		
-		
-		visited = new boolean[N+2][M+2];
-		
-		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=M; j++) {
-				visited[i][j] = true;
-				dfs(i,j, map[i][j], 1);
-				visited[i][j] = false;
-				
-				//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-				etc(i, j, map[i][j]);
-			}
-		}
-		
-		System.out.println(max);
+		System.out.print(sb);
+		br.close();
 	}
 
-	//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-	private static void etc(int r, int c, int sum) {
-		
-		//(r,c) 기준 4방의 값을 모두 더함
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			sum+= map[nr][nc];
-		}
-		
-		//4방의 값을 하나씩 빼보면서 해당 값이 최댓값이라면 갱신
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			max = Math.max(max, sum-map[nr][nc]);
+	private static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+
+		if(x==y) return;
+		if(x<y) {
+			parent[y]=x;
+			size[x]+=size[y];
+		} else {
+			parent[x]=y;
+			size[y]+=size[x];
 		}
 	}
 
-	/**
-	 * 블록하나를 선택하고 다음 블록 선택은 재귀호출로 넘김
-	 * @param r
-	 * @param c
-	 * @param sum	//선택한 블록의 숫자 합
-	 * @param cnt	//선택한 블록의 개수
-	 */
-	private static void dfs(int r, int c, int sum, int cnt) {
-
-		//step01. 4개의 블록 선택 완료
-		if(cnt==4) {
-			//step 02. 선택한 블록 숫자의 합이 최댓값이라면 갱신
-			max = Math.max(sum, max);
-			return;
-		}
-		
-		//윗 방향을 제외한 3방향으로 이동하며 블록 선택
-		for(int d=0; d<3; d++) {
-			
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			
-			//경계를 벗어나거나 이미 방문했다면 다음 방향
-			if(nr>N || nc<1 || nc>M || visited[nr][nc]) continue;
-			
-			visited[nr][nc] = true;
-			dfs(nr, nc, sum+map[nr][nc], cnt+1);
-			visited[nr][nc] = false;
-		}
-		
+	private static int find (int x) {
+		if(parent[x]==x) return x;
+		return parent[x]=find(parent[x]);
 	}
-	
-	
-
 }
