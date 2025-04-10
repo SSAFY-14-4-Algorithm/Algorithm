a13f2b112b9e89e7c337a7e74cf7a1f4a5757f90
ssongmim <108718162+ssongmim@users.noreply.github.com>
2025-04-10 20:44:13 +0900

    Update Baekjoon15486.java

--- week10/Baekjoon15486.java
+++ week10/Baekjoon15486.java
@@ -1,7 +1,47 @@
+import java.io.*;
+import java.util.*;
+/*
+ * 메모리: 313976KB
+ * 시간: 644ms
+ * 
+ * 
+ */
+ 
 
 public class Main {
-
-	public static void main(String[] args) {
-		
+	static int N;
+	static int[] T;
+	static int[] P;
+	static int[] dp;
+	
+	public static void main(String[] args) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	N = Integer.parseInt(br.readLine());
+	
+	T = new int[N];
+	P = new int[N];
+	dp = new int[N+1];
+	Arrays.fill(dp, 0);
+	
+	for(int i=0;i<N;i++) {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		T[i] = Integer.parseInt(st.nextToken());
+		P[i] = Integer.parseInt(st.nextToken());
 	}
+	
+	for(int i=0;i<N;i++) {
+		if(i+T[i]<=N) {
+			dp[i+T[i]] = Math.max(dp[i+T[i]],dp[i]+P[i]);
+		}
+		dp[i+1] = Math.max(dp[i+1],dp[i]);
+	}
+	
+	int res = 0;
+	for(int i=0;i<=N;i++) {
+		res = Math.max(res, dp[i]);
+	}
+	
+	System.out.println(res);
+	}
+	
 }
