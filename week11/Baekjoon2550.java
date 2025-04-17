34adbf43900e4396b5a980fd72c6065917fa95bf
ssongmim <108718162+ssongmim@users.noreply.github.com>
2025-04-17 20:07:54 +0900

    Update Baekjoon2550.java

--- week11/Baekjoon2550.java
+++ week11/Baekjoon2550.java
@@ -1,125 +1,70 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-/**
+import java.io.*;
+import java.util.*;
+/*
+ * 메모리: 20080KB
+ * 시간: 500ms
  * 
- * 각 지점을 중심으로 dfs 아래, 왼쪽, 오른쪽 방향으로만 dfs 탐색 depth:4로
- * 	- 탐색 시 최대값을 발견한다면 갱신
- * 탐색한 지점은 방문 처리
+ * 최장 증가하는 부분 수열, dp(최장증가수열의 길이) 
  * 
  * 
  * 
- * 가운데 모양은 따로 탐색 필요
- * 
- * 
- * @author SSAFY
- *
  */
-public class Main_B_14500_테트로미노_연민호 {
-	
-	//하좌우상
-	final static int[] dr = {1, 0, 0, -1};
-	final static int[] dc = {0, -1, 1, 0};
-	
-	
-	
-	static int N;	//세로 크기
-	static int M;	//가로 크기
-	
-	static int[][] map;	//숫자판 정보
-	
-	static boolean[][] visited;	//방문체크
-	
-	static int max;	//최댓값
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		//탐색의 편이를 위해 공백을 넣어놓음
-		map = new int[N+2][M+2];
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		
-		visited = new boolean[N+2][M+2];
-		
-		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=M; j++) {
-				visited[i][j] = true;
-				dfs(i,j, map[i][j], 1);
-				visited[i][j] = false;
-				
-				//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-				etc(i, j, map[i][j]);
-			}
-		}
-		
-		System.out.println(max);
-	}
+public class Baekjoon2550{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] line1 = new int[N];
+        int[] line2 = new int[N];
+        int[] idx = new int[N + 1];
+        int max = 0;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            line1[i] =Integer.parseInt(st.nextToken());
+            idx[line1[i]] = 1;
+        }
 
-	//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-	private static void etc(int r, int c, int sum) {
-		
-		//(r,c) 기준 4방의 값을 모두 더함
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			sum+= map[nr][nc];
-		}
-		
-		//4방의 값을 하나씩 빼보면서 해당 값이 최댓값이라면 갱신
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			max = Math.max(max, sum-map[nr][nc]);
-		}
-	}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            line2[i] = Integer.parseInt(st.nextToken());
+        }
 
-	/**
-	 * 블록하나를 선택하고 다음 블록 선택은 재귀호출로 넘김
-	 * @param r
-	 * @param c
-	 * @param sum	//선택한 블록의 숫자 합
-	 * @param cnt	//선택한 블록의 개수
-	 */
-	private static void dfs(int r, int c, int sum, int cnt) {
+        int[] idxArr = new int[N]; //2번째 라인의 전구가 첫번째 라인에서 어디서 나오는지 저장 
+        for (int i = 0; i < N; i++) {
+            idxArr[i] = idx[line2[i]];
+        }
 
-		//step01. 4개의 블록 선택 완료
-		if(cnt==4) {
-			//step 02. 선택한 블록 숫자의 합이 최댓값이라면 갱신
-			max = Math.max(sum, max);
-			return;
-		}
-		
-		//윗 방향을 제외한 3방향으로 이동하며 블록 선택
-		for(int d=0; d<3; d++) {
-			
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			
-			//경계를 벗어나거나 이미 방문했다면 다음 방향
-			if(nr>N || nc<1 || nc>M || visited[nr][nc]) continue;
-			
-			visited[nr][nc] = true;
-			dfs(nr, nc, sum+map[nr][nc], cnt+1);
-			visited[nr][nc] = false;
-		}
-		
-	}
-	
-	
+        int[] dp = new int[N];
+        int[] prev = new int[N];
+        Arrays.fill(dp, 1); //최소한 스스로는 포함
+        Arrays.fill(prev, -1);
 
+        int maxLength = 1;
+        int lastIndex = 0;
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (idxArr[j] < idxArr[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > maxLength) {
+                maxLength = dp[i];
+                lastIndex = i;
+            }
+        }
+
+
+       ArrayList<Integer> result = new ArrayList<>(); //역추적 
+        while (lastIndex != -1) {
+            result.add(line2[lastIndex]);
+            lastIndex = prev[lastIndex];
+        }
+        Collections.sort(result);  
+        System.out.println(result.size());
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
 }
