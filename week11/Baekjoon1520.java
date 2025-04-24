228c7754083095a8da4742150da277d8152e6f9c
JUNSU BAE <jsbae1023@gmail.com>
2025-04-24 22:07:14 +0900

    Summit Week11

--- week11/Baekjoon1520.java
+++ week11/Baekjoon1520.java
@@ -1,125 +1,72 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-/**
- * 
- * 각 지점을 중심으로 dfs 아래, 왼쪽, 오른쪽 방향으로만 dfs 탐색 depth:4로
- * 	- 탐색 시 최대값을 발견한다면 갱신
- * 탐색한 지점은 방문 처리
- * 
- * 
- * 
- * 가운데 모양은 따로 탐색 필요
- * 
- * 
- * @author SSAFY
- *
+/*
+ * 메모리: 14,896KB
+ * 시간: 116ms
  */
-public class Main_B_14500_테트로미노_연민호 {
+public class Baekjoon1520 {
+	static StringBuilder sb = new StringBuilder(); 
+	// types
+	static class Node{
+		int y, x, d;
+		public Node(int y, int x, int d) { this.y = y; this.x = x; this.d = d; }
+	}
+	// constants
+	static int SIZE = 500;
+	static int[][] DELTA = {
+			{ 1,  0},
+			{-1,  0},
+			{ 0,  1},
+			{ 0, -1},
+	};
+	// variables
+	static int M, N;
+	static int[][] mat = new int[SIZE][SIZE];
+	static int[][] dp = new int[SIZE][SIZE];
 	
-	//하좌우상
-	final static int[] dr = {1, 0, 0, -1};
-	final static int[] dc = {0, -1, 1, 0};
 	
+	static boolean inRange(int y, int x) {
+		return 0 <= y && y < M && 0 <= x && x < N;
+	}
 	
+	static int dfs(int y, int x) {
+		if(dp[y][x] >= 0) return dp[y][x]; // 이미 도착점까지 도달하는 경로가 계산됨
+		if(y == M - 1 && x == N - 1) return dp[y][x] = 1; // 기저: 도착점 도달
+		
+		dp[y][x] = 0;
+		int cnt = 0;
+		for(int d = 0; d < DELTA.length; ++d) {
+			int nx = x + DELTA[d][0];
+			int ny = y + DELTA[d][1];
+			
+			if(inRange(ny, nx) && mat[y][x] > mat[ny][nx]) {
+				cnt += dfs(ny, nx);
+			}
+		}
+		return dp[y][x] = cnt;
+	}
 	
-	static int N;	//세로 크기
-	static int M;	//가로 크기
-	
-	static int[][] map;	//숫자판 정보
-	
-	static boolean[][] visited;	//방문체크
-	
-	static int max;	//최댓값
+	static int solution() {
+		if(M == 1 && N == 1) return 1;
+		return dfs(0, 0);
+	}
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		//탐색의 편이를 위해 공백을 넣어놓음
-		map = new int[N+2][M+2];
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+		M = readInt(); N = readInt();
+		for(int y = 0; y < M; ++y) {
+			for(int x = 0; x < N; ++x) {
+				mat[y][x] = readInt();
+				dp[y][x] = -1;
 			}
 		}
-		
-		
-		visited = new boolean[N+2][M+2];
-		
-		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=M; j++) {
-				visited[i][j] = true;
-				dfs(i,j, map[i][j], 1);
-				visited[i][j] = false;
-				
-				//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-				etc(i, j, map[i][j]);
-			}
-		}
-		
-		System.out.println(max);
-	}
-
-	//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-	private static void etc(int r, int c, int sum) {
-		
-		//(r,c) 기준 4방의 값을 모두 더함
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			sum+= map[nr][nc];
-		}
-		
-		//4방의 값을 하나씩 빼보면서 해당 값이 최댓값이라면 갱신
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			max = Math.max(max, sum-map[nr][nc]);
-		}
-	}
-
-	/**
-	 * 블록하나를 선택하고 다음 블록 선택은 재귀호출로 넘김
-	 * @param r
-	 * @param c
-	 * @param sum	//선택한 블록의 숫자 합
-	 * @param cnt	//선택한 블록의 개수
-	 */
-	private static void dfs(int r, int c, int sum, int cnt) {
-
-		//step01. 4개의 블록 선택 완료
-		if(cnt==4) {
-			//step 02. 선택한 블록 숫자의 합이 최댓값이라면 갱신
-			max = Math.max(sum, max);
-			return;
-		}
-		
-		//윗 방향을 제외한 3방향으로 이동하며 블록 선택
-		for(int d=0; d<3; d++) {
-			
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			
-			//경계를 벗어나거나 이미 방문했다면 다음 방향
-			if(nr>N || nc<1 || nc>M || visited[nr][nc]) continue;
-			
-			visited[nr][nc] = true;
-			dfs(nr, nc, sum+map[nr][nc], cnt+1);
-			visited[nr][nc] = false;
-		}
-		
-	}
-	
-	
-
+		System.out.print(solution());
+    }
+    
+    static int readInt() throws IOException {
+    	int c, n = 0;
+    	while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+    	if(c == '\r') System.in.read();
+    	return n;
+    }
 }
--- week11/Baekjoon2467.java
+++ week11/Baekjoon2467.java
@@ -1,125 +1,60 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-/**
- * 
- * 각 지점을 중심으로 dfs 아래, 왼쪽, 오른쪽 방향으로만 dfs 탐색 depth:4로
- * 	- 탐색 시 최대값을 발견한다면 갱신
- * 탐색한 지점은 방문 처리
- * 
- * 
- * 
- * 가운데 모양은 따로 탐색 필요
- * 
- * 
- * @author SSAFY
- *
+/*
+ * 메모리: 31,764KB
+ * 시간: 252ms
  */
-public class Main_B_14500_테트로미노_연민호 {
+public class Baekjoon2467 {
+	// Input Handler
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = null;
+	// constants
+	// variables
+	static int N;
+	static long[] potions;
 	
-	//하좌우상
-	final static int[] dr = {1, 0, 0, -1};
-	final static int[] dc = {0, -1, 1, 0};
-	
-	
-	
-	static int N;	//세로 크기
-	static int M;	//가로 크기
-	
-	static int[][] map;	//숫자판 정보
-	
-	static boolean[][] visited;	//방문체크
-	
-	static int max;	//최댓값
+	static long[] solution(long[] potions) {
+		Arrays.sort(potions);
+		if(potions[0] >= 0) {
+			return new long[] {potions[0], potions[1]};
+		} else if(potions[potions.length - 1] <= 0) {
+			return new long[] {potions[potions.length - 2], potions[potions.length - 1]};
+		}
+		
+		
+		int minL = 0, minR = potions.length - 1;
+		long minVal = potions[minL] + potions[minR];
+		{
+			int l = minL, r = minR;
+			while(l + 1 < r) {
+				long val = potions[l] + potions[r];
+				if(val < 0) {
+					++l;
+				} else {
+					--r;
+				}
+				val = potions[l] + potions[r];
+				if(Math.abs(val) < Math.abs(minVal)) {
+					minL = l;
+					minR = r;
+					minVal = val;
+				}
+			}
+		}
+		return new long[] {potions[minL], potions[minR]};
+	}
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		potions = new long[N];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; ++i) potions[i] = Long.parseLong(st.nextToken());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		//탐색의 편이를 위해 공백을 넣어놓음
-		map = new int[N+2][M+2];
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		
-		visited = new boolean[N+2][M+2];
-		
-		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=M; j++) {
-				visited[i][j] = true;
-				dfs(i,j, map[i][j], 1);
-				visited[i][j] = false;
-				
-				//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-				etc(i, j, map[i][j]);
-			}
-		}
-		
-		System.out.println(max);
+		long[] ret = solution(potions);
+		bw.append(ret[0] + " " + ret[1]).flush();
 	}
-
-	//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-	private static void etc(int r, int c, int sum) {
-		
-		//(r,c) 기준 4방의 값을 모두 더함
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			sum+= map[nr][nc];
-		}
-		
-		//4방의 값을 하나씩 빼보면서 해당 값이 최댓값이라면 갱신
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			max = Math.max(max, sum-map[nr][nc]);
-		}
-	}
-
-	/**
-	 * 블록하나를 선택하고 다음 블록 선택은 재귀호출로 넘김
-	 * @param r
-	 * @param c
-	 * @param sum	//선택한 블록의 숫자 합
-	 * @param cnt	//선택한 블록의 개수
-	 */
-	private static void dfs(int r, int c, int sum, int cnt) {
-
-		//step01. 4개의 블록 선택 완료
-		if(cnt==4) {
-			//step 02. 선택한 블록 숫자의 합이 최댓값이라면 갱신
-			max = Math.max(sum, max);
-			return;
-		}
-		
-		//윗 방향을 제외한 3방향으로 이동하며 블록 선택
-		for(int d=0; d<3; d++) {
-			
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			
-			//경계를 벗어나거나 이미 방문했다면 다음 방향
-			if(nr>N || nc<1 || nc>M || visited[nr][nc]) continue;
-			
-			visited[nr][nc] = true;
-			dfs(nr, nc, sum+map[nr][nc], cnt+1);
-			visited[nr][nc] = false;
-		}
-		
-	}
-	
-	
-
 }
+	
