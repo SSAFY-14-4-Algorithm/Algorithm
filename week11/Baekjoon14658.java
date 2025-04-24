e83c17fc8a8d25cff48fb3046ab06124017749fe
JUNSU BAE <jsbae1023@gmail.com>
2025-04-24 22:07:14 +0900

    Summit Week11

--- week11/Baekjoon14658.java
+++ week11/Baekjoon14658.java
@@ -1,125 +1,57 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-/**
- * 
- * 각 지점을 중심으로 dfs 아래, 왼쪽, 오른쪽 방향으로만 dfs 탐색 depth:4로
- * 	- 탐색 시 최대값을 발견한다면 갱신
- * 탐색한 지점은 방문 처리
- * 
- * 
- * 
- * 가운데 모양은 따로 탐색 필요
- * 
- * 
- * @author SSAFY
- *
+/*
+ * 메모리: 11,740KB
+ * 시간: 84ms
  */
-public class Main_B_14500_테트로미노_연민호 {
+public class Baekjoon14658 {
+	// IO Handler
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StreamTokenizer st = null;
+	// types
+	// constants
+	static final int MAX_K = 100;
+	// variables
+	static int N, M, L, K;
+	static int[] X = new int[MAX_K];
+	static int[] Y = new int[MAX_K];
 	
-	//하좌우상
-	final static int[] dr = {1, 0, 0, -1};
-	final static int[] dc = {0, -1, 1, 0};
 	
+	static boolean overlap(int ay, int ax, int y, int x) {
+		return 0 <= ay - y && ay - y <= L && 0 <= ax - x && ax - x <= L;
+	}
 	
-	
-	static int N;	//세로 크기
-	static int M;	//가로 크기
-	
-	static int[][] map;	//숫자판 정보
-	
-	static boolean[][] visited;	//방문체크
-	
-	static int max;	//최댓값
+	static int solution() {
+		int max = 0;
+		for(int ix = 0; ix < K; ++ix) {
+			int x = X[ix];
+			for(int iy = 0; iy < K; ++iy) {
+				int y = Y[iy];
+				
+				int cnt = 0;
+				for(int i = 0; i < K; ++i) {
+					if(overlap(y, x, Y[i], X[i])) ++cnt;
+				}
+				max = Math.max(max, cnt);
+			}
+		}
+		return max;
+	}
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		//탐색의 편이를 위해 공백을 넣어놓음
-		map = new int[N+2][M+2];
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
+		N = readInt(); M = readInt(); L = readInt(); K = readInt();
+		for(int i = 0; i < K; ++i) {
+			X[i] = readInt(); Y[i] = readInt();
 		}
-		
-		
-		visited = new boolean[N+2][M+2];
-		
-		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=M; j++) {
-				visited[i][j] = true;
-				dfs(i,j, map[i][j], 1);
-				visited[i][j] = false;
-				
-				//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-				etc(i, j, map[i][j]);
-			}
-		}
-		
-		System.out.println(max);
-	}
-
-	//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-	private static void etc(int r, int c, int sum) {
-		
-		//(r,c) 기준 4방의 값을 모두 더함
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			sum+= map[nr][nc];
-		}
-		
-		//4방의 값을 하나씩 빼보면서 해당 값이 최댓값이라면 갱신
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			max = Math.max(max, sum-map[nr][nc]);
-		}
-	}
-
-	/**
-	 * 블록하나를 선택하고 다음 블록 선택은 재귀호출로 넘김
-	 * @param r
-	 * @param c
-	 * @param sum	//선택한 블록의 숫자 합
-	 * @param cnt	//선택한 블록의 개수
-	 */
-	private static void dfs(int r, int c, int sum, int cnt) {
-
-		//step01. 4개의 블록 선택 완료
-		if(cnt==4) {
-			//step 02. 선택한 블록 숫자의 합이 최댓값이라면 갱신
-			max = Math.max(sum, max);
-			return;
-		}
-		
-		//윗 방향을 제외한 3방향으로 이동하며 블록 선택
-		for(int d=0; d<3; d++) {
-			
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			
-			//경계를 벗어나거나 이미 방문했다면 다음 방향
-			if(nr>N || nc<1 || nc>M || visited[nr][nc]) continue;
-			
-			visited[nr][nc] = true;
-			dfs(nr, nc, sum+map[nr][nc], cnt+1);
-			visited[nr][nc] = false;
-		}
-		
+		System.out.print(K-solution());
 	}
 	
-	
-
+	static int readInt() throws IOException {
+		int c, n = 0;
+		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+		//if(c == '\r') System.in.read();
+		return n;
+	}
 }
--- week11/Baekjoon2550.java
+++ week11/Baekjoon2550.java
@@ -1,125 +1,87 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-/**
- * 
- * 각 지점을 중심으로 dfs 아래, 왼쪽, 오른쪽 방향으로만 dfs 탐색 depth:4로
- * 	- 탐색 시 최대값을 발견한다면 갱신
- * 탐색한 지점은 방문 처리
- * 
- * 
- * 
- * 가운데 모양은 따로 탐색 필요
- * 
- * 
- * @author SSAFY
- *
+/*
+ * 메모리: 14,080KB
+ * 시간: 108ms
  */
-public class Main_B_14500_테트로미노_연민호 {
+public class Baekjoon2550 {
+	// Input Handler
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	// types
+	// constants
+	static final int SIZE =  10_000;
+	// variables
+	static int N;
+	static int[] switchToNo = new int[SIZE];
+	static int[] bulbToNo = new int[SIZE];
+	//static int[] noToSwitch = new int[SIZE+1];
+	static int[] noToBulb = new int[SIZE+1];
 	
-	//하좌우상
-	final static int[] dr = {1, 0, 0, -1};
-	final static int[] dc = {0, -1, 1, 0};
+	static int[] A = new int[SIZE];
+	static int len = 0;
+	static int[] buffer = new int[SIZE];
+	static int[] dp = new int[SIZE];
+	static int[] lis = new int[SIZE];
 	
+	/*
+	 * A[i]: i번째 스위치에 대응하는 전구의 위치
+	 * LIS: A로 생성한 LIS
+	 * LIS[i]를 번호로 매핑한 값이 최종 결과
+	 */
 	
+	static int lowerBound(int lo, int hi, int key) {
+		while(lo < hi) {
+			int mid = (lo + hi) >> 1;
+			if(buffer[mid] < key) lo = mid + 1;
+			else hi = mid;
+		}
+		return lo;
+	}
 	
-	static int N;	//세로 크기
-	static int M;	//가로 크기
-	
-	static int[][] map;	//숫자판 정보
-	
-	static boolean[][] visited;	//방문체크
-	
-	static int max;	//최댓값
+	static void solution() {
+		for(int i = 0; i < N; ++i) {
+			int at = lowerBound(0, len, A[i]);
+			if(at == len) ++len;
+			buffer[at] = A[i];
+			dp[i] = at;
+		}
+		for(int i = N - 1, j = len - 1; j >= 0; --i) {
+			if(dp[i] == j) {
+				lis[j--] = bulbToNo[A[i]];
+			}
+		}
+		Arrays.sort(lis, 0, len);
+	}
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		//탐색의 편이를 위해 공백을 넣어놓음
-		map = new int[N+2][M+2];
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
+		// Input
+		N = readInt();
+		for(int i = 0; i < N; ++i) {
+			switchToNo[i] = readInt();
+			//noToSwitch[switchToNo[i]] = i;
 		}
-		
-		
-		visited = new boolean[N+2][M+2];
-		
-		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=M; j++) {
-				visited[i][j] = true;
-				dfs(i,j, map[i][j], 1);
-				visited[i][j] = false;
-				
-				//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-				etc(i, j, map[i][j]);
-			}
+		for(int i = 0; i < N; ++i) {
+			bulbToNo[i] = readInt();
+			noToBulb[bulbToNo[i]] = i;
 		}
+		for(int i = 0; i < N; ++i) A[i] = noToBulb[switchToNo[i]];
 		
-		System.out.println(max);
-	}
-
-	//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-	private static void etc(int r, int c, int sum) {
+		// solution
+		solution();
 		
-		//(r,c) 기준 4방의 값을 모두 더함
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			sum+= map[nr][nc];
-		}
-		
-		//4방의 값을 하나씩 빼보면서 해당 값이 최댓값이라면 갱신
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			max = Math.max(max, sum-map[nr][nc]);
-		}
-	}
-
-	/**
-	 * 블록하나를 선택하고 다음 블록 선택은 재귀호출로 넘김
-	 * @param r
-	 * @param c
-	 * @param sum	//선택한 블록의 숫자 합
-	 * @param cnt	//선택한 블록의 개수
-	 */
-	private static void dfs(int r, int c, int sum, int cnt) {
-
-		//step01. 4개의 블록 선택 완료
-		if(cnt==4) {
-			//step 02. 선택한 블록 숫자의 합이 최댓값이라면 갱신
-			max = Math.max(sum, max);
-			return;
-		}
-		
-		//윗 방향을 제외한 3방향으로 이동하며 블록 선택
-		for(int d=0; d<3; d++) {
-			
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			
-			//경계를 벗어나거나 이미 방문했다면 다음 방향
-			if(nr>N || nc<1 || nc>M || visited[nr][nc]) continue;
-			
-			visited[nr][nc] = true;
-			dfs(nr, nc, sum+map[nr][nc], cnt+1);
-			visited[nr][nc] = false;
-		}
-		
+		// output
+		sb.append(len).append("\n");
+		for(int i = 0; i < len; ++i) sb.append(lis[i]).append(" ");
+		System.out.print(sb);
 	}
 	
-	
-
+	static int readInt() throws IOException{
+		int c, n = 0;
+		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+		if(c == '\r') System.in.read();
+		return n;
+	}
 }
