5da9da29b1c989ff716b8779a92bb87081c04706
jung980514 <jung980514@naver.com>
2025-04-24 21:00:11 +0900

    Update week12

---
+++ week12/Baekjoon16637.java
@@ -0,0 +1,50 @@
+import java.io.*;
+/*
+ * 메모리 11528KB
+ * 시간 64ms
+ */
+public class Baekjoon16637 {
+    static int N;
+    static char[] expression;
+    static int maxResult = Integer.MIN_VALUE;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // static 선언
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine()); 
+        expression = br.readLine().toCharArray(); 
+
+        dfs(0, expression[0] - '0'); // DFS 탐색 시작
+        System.out.println(maxResult); // 결과 출력
+    }
+
+    // DFS 탐색 함수
+    static void dfs(int index, int currentResult) {
+        if (index >= N - 1) { // 수식 끝까지 탐색 완료
+            maxResult = Math.max(maxResult, currentResult);
+            return;
+        }
+
+        // 괄호 없이 계산
+        char operator = expression[index + 1];
+        int nextNum = expression[index + 2] - '0';
+        dfs(index + 2, calculate(currentResult, nextNum, operator));
+
+        // 괄호 추가 계산 (index+4까지 가능할 때만)
+        if (index + 4 < N) {
+            char nextOperator = expression[index + 3];
+            int afterNextNum = expression[index + 4] - '0';
+            int bracketResult = calculate(nextNum, afterNextNum, nextOperator);
+            dfs(index + 4, calculate(currentResult, bracketResult, operator));
+        }
+    }
+
+    // 연산 함수
+    static int calculate(int a, int b, char operator) {
+        switch (operator) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+        }
+        return 0; 
+    }
+}
---
+++ week12/Baekjoon19539.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+/*
+ * 메모리 23052KB
+ * 시간 208ms
+ */
+public class Baekjoon19539 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		String[] s = br.readLine().split(" ");
+		int sum=0;
+		int count1=0;
+		int count2=0;
+		for(int i=0;i<n;i++) {
+			sum+=Integer.parseInt(s[i]);
+			count1+=Integer.parseInt(s[i])%2;
+			count2+=Integer.parseInt(s[i])/2;
+		}
+		
+		if(sum%3>0 || count1>count2) {
+			System.out.println("NO");
+		}
+		else {
+			System.out.println("YES");
+		}
+	}
+}
