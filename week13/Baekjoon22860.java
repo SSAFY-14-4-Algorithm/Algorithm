93d16ef9437c42d58b223ba379e6e2f6d3356574
JUNSU BAE <jsbae1023@gmail.com>
2025-05-01 22:13:36 +0900

    typo

--- week13/Baekjoon22860.java
+++ week13/Baekjoon22860.java
@@ -1,125 +1,85 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
- * 
- * 각 지점을 중심으로 dfs 아래, 왼쪽, 오른쪽 방향으로만 dfs 탐색 depth:4로
- * 	- 탐색 시 최대값을 발견한다면 갱신
- * 탐색한 지점은 방문 처리
- * 
- * 
- * 
- * 가운데 모양은 따로 탐색 필요
- * 
- * 
- * @author SSAFY
- *
+ * 폴더 정리 (small), 22860
+ * 메모리: 88,464KB
+ * 시간: 544ms
  */
-public class Main_B_14500_테트로미노_연민호 {
+public class Baekjoon22860 {
+	// Input Handler
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st = null;
+	// types
+	static class Node{
+		List<Node> subDict = new ArrayList<Node>(); // 바로 아래에 위치한 폴더
+		Set<String> fileNames = new HashSet<>(); // 현재 폴더 이하의 경로에 위치한 파일의 이름들
+		int fileNum; // 현재 폴더 이하의 경로에 위치한 파일의 수
+	}
+	// constants
+	// variables
+	static int N, M, Q;
+	static Node root;
+	// 디렉토리 이름에는 중복이 없다. -> 디렉토리 이름과 인덱스는 1:1 대응관게가 성립
+	static Map<String, Node> mapping = new HashMap<String, Node>();
 	
-	//하좌우상
-	final static int[] dr = {1, 0, 0, -1};
-	final static int[] dc = {0, -1, 1, 0};
 	
+	static Node getNode(String name) {
+		Node node = mapping.getOrDefault(name, null); 
+		if(node == null) mapping.put(name, node = new Node());
+		return node;
+	}
 	
+	static void insert(String P, String F, int C) {
+		Node nodeP = getNode(P);
+		if(C == 0) { // F가 파일
+			++nodeP.fileNum;
+			nodeP.fileNames.add(F);
+		} else { // F가 폴더
+			Node nodeF = getNode(F);
+			nodeP.subDict.add(nodeF);
+		}
+	}
 	
-	static int N;	//세로 크기
-	static int M;	//가로 크기
+	static void build(Node root) {
+		for(Node next : root.subDict) {
+			build(next);
+			root.fileNum += next.fileNum;
+			root.fileNames.addAll(next.fileNames);
+		}
+	}
 	
-	static int[][] map;	//숫자판 정보
-	
-	static boolean[][] visited;	//방문체크
-	
-	static int max;	//최댓값
+	static int[] query(String[] path) {
+		String target = path[path.length - 1];
+		Node node = mapping.get(target);
+		return new int[] {node.fileNames.size(),   node.fileNum};
+	}
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		root = new Node();
+		mapping.put("main", root);
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+		// 구성
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
-		//탐색의 편이를 위해 공백을 넣어놓음
-		map = new int[N+2][M+2];
-		for(int i=1; i<=N; i++) {
+		for(int i = 0; i < N + M; ++i) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		
-		visited = new boolean[N+2][M+2];
-		
-		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=M; j++) {
-				visited[i][j] = true;
-				dfs(i,j, map[i][j], 1);
-				visited[i][j] = false;
-				
-				//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-				etc(i, j, map[i][j]);
-			}
-		}
-		
-		System.out.println(max);
-	}
-
-	//가운데 블록 기준 3개 블록이 붙어있는 테트로미노 탐색
-	private static void etc(int r, int c, int sum) {
-		
-		//(r,c) 기준 4방의 값을 모두 더함
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			sum+= map[nr][nc];
-		}
-		
-		//4방의 값을 하나씩 빼보면서 해당 값이 최댓값이라면 갱신
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			max = Math.max(max, sum-map[nr][nc]);
-		}
-	}
-
-	/**
-	 * 블록하나를 선택하고 다음 블록 선택은 재귀호출로 넘김
-	 * @param r
-	 * @param c
-	 * @param sum	//선택한 블록의 숫자 합
-	 * @param cnt	//선택한 블록의 개수
-	 */
-	private static void dfs(int r, int c, int sum, int cnt) {
-
-		//step01. 4개의 블록 선택 완료
-		if(cnt==4) {
-			//step 02. 선택한 블록 숫자의 합이 최댓값이라면 갱신
-			max = Math.max(sum, max);
-			return;
-		}
-		
-		//윗 방향을 제외한 3방향으로 이동하며 블록 선택
-		for(int d=0; d<3; d++) {
+			String P = st.nextToken();
+			String F = st.nextToken();
+			int C = Integer.parseInt(st.nextToken());
 			
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			
-			//경계를 벗어나거나 이미 방문했다면 다음 방향
-			if(nr>N || nc<1 || nc>M || visited[nr][nc]) continue;
-			
-			visited[nr][nc] = true;
-			dfs(nr, nc, sum+map[nr][nc], cnt+1);
-			visited[nr][nc] = false;
+			insert(P, F, C);
 		}
-		
+		build(root);
+		// 쿼리
+		Q = Integer.parseInt(br.readLine());
+		while(Q-- > 0) {
+			String[] path = br.readLine().split("/");
+			int[] ans = query(path); 
+			sb.append(ans[0]).append(" ").append(ans[1]).append("\n");
+		}
+		System.out.print(sb);
 	}
-	
-	
-
 }
