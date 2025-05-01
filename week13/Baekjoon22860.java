18e047bf6d97fc50e7605cdc9fe076572669718c
ssongmim <108718162+ssongmim@users.noreply.github.com>
2025-05-01 21:11:00 +0900

    Update Baekjoon22860.java

--- week13/Baekjoon22860.java
+++ week13/Baekjoon22860.java
@@ -15,17 +15,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-        // 입력 처리
         st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 폴더 수
-        int M = Integer.parseInt(st.nextToken()); // 파일 수
-
-        // 루트 폴더 생성
+        int N = Integer.parseInt(st.nextToken()); 
+        int M = Integer.parseInt(st.nextToken()); 
         Folder root = new Folder("main");
         folderMap.put("main", root);
 
-        // 폴더 및 파일 정보 입력
         for (int i = 0; i < N + M; i++) {
             st = new StringTokenizer(br.readLine());
             String parentName = st.nextToken();
@@ -44,7 +39,6 @@ public class Main {
             }
         }
 
-        // 쿼리 처리
         int Q = Integer.parseInt(br.readLine());
         for (int i = 0; i < Q; i++) {
             String path = br.readLine();
@@ -60,7 +54,6 @@ public class Main {
         }
     }
 
-    // DFS를 통해 하위 폴더의 파일 정보를 수집
     static void dfs(Folder folder, Set<String> uniqueFiles) {
         for (String file : folder.files) {
             uniqueFiles.add(file);
