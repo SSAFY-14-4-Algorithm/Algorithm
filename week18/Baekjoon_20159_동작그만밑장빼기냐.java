327e0d67f7a3b6d9200e019d4ae923869d765596
JUNSU BAE <jsbae1023@gmail.com>
2025-06-26 17:03:51 +0900

    Week18 제출

---
+++ week18/Baekjoon_20159_동작그만밑장빼기냐.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.BiFunction;
+
+/**
+ * https://www.acmicpc.net/problem/20159
+ * 시간: 100ms
+ * 메모리: 13296KB
+ */
+public class Baekjoon_20159_동작그만밑장빼기냐 {
+	// Input Handler
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st = null;
+	// types
+	// constants
+	// variables
+	static int N;
+	static int[] X;
+	static int[] leftSum, rightSum;
+	
+	public static void main(String[] args) throws IOException {
+		N = readInt();
+		X = new int[N];
+		for(int i = 0; i < N; ++i) X[i] = readInt();
+		
+		leftSum = new int[N+1];  // [i]: Sum(X[j % 2 == 0 && j < i]) 
+		rightSum = new int[N+1]; // [i]: Sum(X[j % 2 == 1 && j >= N - i])
+		
+		leftSum[0] = 0;
+		for(int i = 2; i <= N; i += 2) {
+			leftSum[i - 1] = leftSum[i] = leftSum[i - 2] + X[i - 2];   
+		}
+		rightSum[0] = 0;
+		for(int i = 2; i <= N; i += 2) {
+			rightSum[i - 1] = rightSum[i] = rightSum[i - 2] + X[N - i + 1]; 
+		}
+		
+		int max = Integer.MIN_VALUE;
+		for(int i = 0; i <= N; ++i) {
+			int sum = leftSum[i] + rightSum[N - i];
+			if(i % 2 == 1) sum -= X[N - 1];
+			max = Math.max(max, sum);
+		}
+		System.out.print(max);
+	}
+	
+	static int readInt() throws IOException {
+		int c, n = 0;
+		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+		if(c == '\r') System.in.read();
+		return n;
+	}
+}
---
+++ week18/Baekjoon_2170_선긋기.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * https://www.acmicpc.net/problem/2170
+ * 시간: 1032ms
+ * 메모리: 49316KB
+ */
+public class Baekjoon_2170_선긋기 {
+	// Input Handler
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st = null;
+	// types
+	static class Line implements Comparable<Line> {
+		int x, y;
+		
+		Line() { }
+		Line(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public int compareTo(Line other) {
+			return this.x - other.x;
+		}
+	}
+	// constants
+	// variables
+	static int N, Q;
+	static PriorityQueue<Line> pq = new PriorityQueue<>();
+	static int[] ids;
+	
+	
+	public static void main(String[] args) throws IOException {
+		N = readInt();
+		
+		for(int i = 0; i < N; ++i) {
+			pq.offer(new Line(readInt(), readInt()));
+		}
+		
+		int sum = 0;
+		int lastPos = Integer.MIN_VALUE;
+		while(!pq.isEmpty()) {
+			Line line = pq.poll();
+			
+			if(lastPos < line.x) { // 선이 이어지지지 않음
+				sum += line.y - line.x;
+			} else if(line.y > lastPos){ // 선이 이어졌고 길이가 연장됨
+				sum += line.y - lastPos;
+			}
+			lastPos = Math.max(lastPos, line.y);
+		}
+		
+		
+		System.out.print(sum);
+	}
+	
+	static int readInt() throws IOException {
+		int c, n = System.in.read() & 0x0F, s = 1;
+		if(n == ('-' & 0x0F)) {
+			s = -1;
+			n = 0;
+		}
+		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+		if(c == '\r') System.in.read();
+		return n * s;
+	}
+}
