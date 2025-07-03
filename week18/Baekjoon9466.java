172996e61296422e29122c1b79f012fabca94fc6
이아현 <lah930@naver.com>
2025-07-03 21:26:15 +0900

    코드 제출

--- week18/Baekjoon9466.java
+++ week18/Baekjoon9466.java
@@ -3,8 +3,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Baekjoon9466 {
-
-    //메모리 24580 kb 시간 272 ms
+    
     public static void main(String[] args) throws Exception {
 
     }
