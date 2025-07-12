1820e80795483630324617423f77900c957d7c74
Hyewon Lee <dahxtq1@naver.com>
2025-07-12 20:00:56 +0900

    ✨ 1464 뒤집기3 혜원

--- week20/Baekjoon1464.java
+++ week20/Baekjoon1464.java
@@ -1,3 +1,23 @@
-public class Baekjoon1516 {
+package m07.d12;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_1464_뒤집기3 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        ArrayDeque<Character> q = new ArrayDeque<>();
+        q.offer(S.charAt(0));
+        for (int i=1;i<S.length();i++){
+            char s = S.charAt(i);
+            if (s <= q.getFirst()) q.offerFirst(s);
+            else q.offerLast(s);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char s : q) sb.append(s);
+        System.out.print(sb);
+    }
 
 }
